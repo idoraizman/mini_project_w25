092b3aa623d13767bc8411f4efca99977f94e304
idoraizman <ido.raizman@campus.technion.ac.il>
2025-03-10 12:03:48 +0200

    prints

--- code/main.py
+++ code/main.py
@@ -88,10 +88,13 @@ class MnistDecoderCNN(nn.Module):
         #  inputs to the Encoder were.
         # ====== YOUR CODE: ======
         modules.append(nn.Linear(in_features=128, out_features=6400, bias=True, device=self.device))
+        print("pre batch norm")
         modules.append(nn.BatchNorm1d(6400))
+        print("after batch norm")
         modules.append(nn.PReLU())
         modules.append(nn.Dropout(0.4))
         modules.append(nn.Unflatten(1, (10, 10, 64)))
+        print("after flatten")
 
         modules.append(nn.ConvTranspose2d(64, 32, kernel_size=3))
         modules.append(nn.BatchNorm2d(32))
