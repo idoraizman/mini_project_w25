af76bf26d053cc62377cf03e63cbc41e434d4341
idoraizman <ido.raizman@campus.technion.ac.il>
2025-03-10 13:37:09 +0200

    script

---
+++ code/myscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Setup env
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate aes
+echo "hello from $(python --version) in $(which python)"
+
+# Run some arbitrary python
+python3 main.py
