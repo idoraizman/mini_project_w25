8af97bb8c74dcc00d4a6a746ba90d588128c9375
idoraizman <ido.raizman@campus.technion.ac.il>
2025-03-28 11:15:59 +0200

    tuning cifar supervised lr_cl

--- code/main.py
+++ code/main.py
@@ -706,7 +706,7 @@ def tune_hp(args, transform):
     temperatures = [0.5] if args.simclr else [0.5]
     for temperature in temperatures:
         for lr_ae in [0.0002]:
-            for lr_cl in [0.0008, 0.002, 0.005]:
+            for lr_cl in [0.0006, 0.0008, 0.001]:
                 for dropout in [0.2]:
                     for batch_size in [64]:
 
