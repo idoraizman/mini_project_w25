ce244873d935ae9090532da74a82f32cf1d0e9f9
Adam Gaber <adam.gaber@campus.technion.ac.il>
2025-03-28 23:35:10 +0300

    unsupervised lr_ae validation

--- code/main.py
+++ code/main.py
@@ -708,7 +708,7 @@ def tune_hp(args, transform):
             checkpoint_ae = None
             for lr_cl in [0.00004]:
                 for dropout in [0.2]:
-                    for batch_size in [128]:
+                    for batch_size in [512] if args.simclr else [128]:
 
                         args.lr_ae = lr_ae
                         args.lr_cl = lr_cl
