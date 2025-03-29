b29c15c362d1a13ecc574803dedf2d1719e4b45f
idoraizman <ido.raizman@campus.technion.ac.il>
2025-03-29 13:32:37 +0200

    tuning simclr lr_cl

--- code/main.py
+++ code/main.py
@@ -707,7 +707,7 @@ def tune_hp(args, transform):
     for temperature in temperatures:
         for lr_ae in [0.00005]:
             checkpoint_ae = None
-            for lr_cl in [0.00005, 0.0005, 0.0008]:
+            for lr_cl in [0.00005, 0.00008, 0.0002, 0.0005, 0.0008, 0.002]:
                 for dropout in [0.2]:
                     for batch_size in [64]:
 
