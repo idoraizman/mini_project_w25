a1b2f9271d01e5e215d9c96f8e53b3648de2c989
Adam Gaber <adam.gaber@campus.technion.ac.il>
2025-03-29 03:06:09 +0300

    unsupervised lr_ae validation

--- code/main.py
+++ code/main.py
@@ -704,9 +704,9 @@ def tune_hp(args, transform):
     best_hp = {}
     temperatures = [0.5] if args.simclr else [0.5]
     for temperature in temperatures:
-        for lr_ae in [0.00001]:
+        for lr_ae in [0.0001, 0.00001, 0.000001]:
             checkpoint_ae = None
-            for lr_cl in [0.001, 0.0001, 0.00005, 0.00001]:
+            for lr_cl in [0.00005]:
                 for dropout in [0.2]:
                     for batch_size in [128]:
 
@@ -784,7 +784,7 @@ if __name__ == "__main__":
 
     print("Device:", args.device)
     if args.val:
-        args.epochs = 200
+        args.epochs = 100
         tune_hp(args, transform)
         exit()
 
