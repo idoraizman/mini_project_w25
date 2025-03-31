6aff8d8eb0bcf3225cee225fff4a36927c3886d5
Adam Gaber <adam.gaber@campus.technion.ac.il>
2025-03-31 10:02:34 +0300

    unsupervised lr_ae validation

--- code/main.py
+++ code/main.py
@@ -789,11 +789,11 @@ if __name__ == "__main__":
         tune_hp(args, transform)
         exit()
 
-    lr_ae = 0.00001
-    lr_cl =  0.0008
+    lr_ae = 0.0001
+    lr_cl =  0.0002
     dropout = 0.2
     batch_size = 64
-    temperature = 0.75
+    temperature = 0.15
 
     args.lr_ae = lr_ae
     args.lr_cl = lr_cl
